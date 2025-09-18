6091937bf2284096188790a2e817dbecf871efff
Pedro Tiago <pedro05112@gmail.com>
2025-09-18 15:22:19 -0300

    Iniciando o projeto

---
+++ Odisseia/cadastro/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Usuario
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'usuarios/home.html')
+
+def usuarios(request):
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.email = request.POST.get('email')
+    novo_usuario.senha = request.POST.get('senha')
+    novo_usuario.save()
+
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    return render(request, 'usuarios/usuarios.html', usuarios)
