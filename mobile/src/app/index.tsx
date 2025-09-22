830bb4f157b6580d6747d86c7b9a82741bd34bb5
Pedro Tiago <pedro05112@gmail.com>
2025-09-22 17:44:53 -0300

    Corrigindo o erro dos arquivos de configuração

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,22 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 export default function Index() {
     return (
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello World</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 32,
+        justifyContent: "center",
+    },
+    title: {
+        color: "red",
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+})
