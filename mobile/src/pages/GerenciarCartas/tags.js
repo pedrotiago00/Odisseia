ba17af3655ccbd7281f158d25a6a2d460765ef11
Luiz <lf45818@gmail.com>
2025-11-10 13:14:25 -0300

    Tags adiconadas

--- mobile/src/pages/GerenciarCartas/tags.js
+++ mobile/src/pages/GerenciarCartas/tags.js
@@ -1,6 +1,20 @@
 export const FILTROSTag = [
     { nome: 'Todos', tag: 'all' },
-    { nome: 'Magia', tag: 'magia' },
+
+    { nome: 'Campeões', tag: 'campeoes' },
+    { nome: 'Subclasses', tag: 'subclasses' },
+    { nome: 'Perícias', tag: 'pericias' },
+    { nome: 'PROMOS', tag: 'promos' },
+    { nome: 'Aliados', tag: 'aliados' },
+    { nome: 'Estruturas', tag: 'estruturas' },
+    { nome: 'Épicas', tag: 'epicas' },
+    { nome: 'Armadilhas', tag: 'armadilhas' },
+    { nome: 'Armas/Escudos', tag: 'armas_escudos' },
+    { nome: 'Equipamentos', tag: 'equipamentos' },
+    { nome: 'Itens', tag: 'itens' },
+    { nome: 'Cenários', tag: 'cenarios' },
+    { nome: 'Outros', tag: 'outros' },
+
     { nome: 'Ataque', tag: 'ataque' },
     { nome: 'Suporte', tag: 'suporte' },
     { nome: 'MessiCareca', tag: 'messicareca' },
